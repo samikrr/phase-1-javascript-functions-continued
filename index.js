ba88f2c7beaad64baa65a00d08d104e7ef7a1b0d
Samuel Kirori <samuelkirori300@gmail.com>
2024-03-30 13:11:45 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun("bathes my dog")
+
+function mondayWork(activity="go to the office"){
+    return (`This Monday, I will ${activity}.`);
+}
+mondayWork("work from home")
+
+function wrapAdjective(flair="*"){
+    return function(adjective="Special"){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+const encouragingPromptFunction=wrapAdjective("!!!")
+    // return encouragingPromptFunction("a dedicated programmer")
+}
+wrapAdjective("%")("a dedicated programmer");
